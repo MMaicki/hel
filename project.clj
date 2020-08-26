429fee1952a4cafad54b6bea8ef71b8a3db97972
Marcin <marcin.maicki@nextlaw.com>
2020-08-26 15:57:24 +0200

    Added migratus and migrations to project

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  [re-frame "1.0.0"]
                  [com.stuartsierra/component "1.0.0"]
                  [hikari-cp "2.13.0"]
+                 [migratus "1.2.8"]
                  [io.aviso/logging "0.2.0"]
                  [clj-commons/secretary "1.2.4"]
                  [venantius/accountant "0.2.5"]
@@ -21,7 +22,10 @@
                  [ring/ring-json "0.5.0"]
                  [ring/ring-defaults "0.3.2"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [org.postgresql/postgresql "9.4-1201-jdbc41"] ]
+                 [org.postgresql/postgresql "42.2.14"]]
+  :migratus {:store         :database
+             :migration-dir "migrations"
+             :db            "postgresql://localhost:5432/hel?user=postgres&password=nxCXwqKhfdw4o2"}
   :cljsbuild {:builds [{:id           "app"
                         :source-paths ["src"]
                         :figwheel     {:on-jsload     "on-js-reload"
@@ -35,9 +39,9 @@
              :server-port      3449
              :ring-handler     hel.handler/app}
   :plugins [[lein-ring "0.12.5"]
+            [migratus-lein "0.7.3"]
             [lein-figwheel "0.5.20"]]
   :ring {:handler hel.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]
-                        [re-frisk "1.3.4"]]}})
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]
+                                  [re-frisk "1.3.4"]]}})
